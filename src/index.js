8c78588cf09548b92a02d44193435e3fb15639ba
ricardobumu <ricardobumu@gmail.com>
2025-07-08 15:06:52 +0200

    Actualización de archivos

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo desde Express!');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en puerto 3000');
+});
